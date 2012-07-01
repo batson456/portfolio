d4667488a50cf0752cf9223a7d1667a34572c487
batson456 <batson456@gmail.com>
2012-07-01 17:12:18 -0400

    Integrate the twitter feed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  @twitter_feed = Twitter.user_timeline("batson456").first.text
 end
--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 
+require 'twitter'
 require 'rails/all'
 
 if defined?(Bundler)
