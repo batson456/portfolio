db4634e626ddcc6eb8a98996a9af1716ad93e7b1
batson456 <batson456@gmail.com>
2012-07-23 13:31:44 -0400

    Merge modify-javascript and delete contact form

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,23 +1,3 @@
 class Message
 
-  include ActiveModel::Validations
-  include ActiveModel::Conversion
-  include ActiveModel::Naming
-
-  attr_accessible :email, :message, :name
-
-  validates :name, :email, :message, :presence => true
-  validates :email, :format => { :with => %r{.+@.+\..+} }, :allow_blank => true
-
-  def initialize(attributes = {})
-    attributes.each do |name, value|
-      send("#{name}=", value)
-    end
-  end
-
-  def persisted?
-    false
-  end
-
-
 end
