92e453877ba086c43387f4022b760c09c553fdff
batson456 <batson456@gmail.com>
2012-07-26 23:18:30 -0400

    Remove javascript from scripts.js

--- app/assets/javascripts/scripts.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    // hide the div as soon as the DOM is ready
-    $('.contact_container').hide();
-    $('.toggle').show();
-});
-
-function toggle_div(link){
-    $(link).toggle('slow');
-}
