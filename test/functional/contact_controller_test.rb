da7b25cea95d8bb9ff2d589b0959dc6e4f34e9de
batson456 <batson456@gmail.com>
2012-07-26 22:07:57 -0400

    Add contact controller and email view

--- test/functional/contact_controller_test.rb
+++ test/functional/contact_controller_test.rb
@@ -1,9 +1,7 @@
 require 'test_helper'
 
 class ContactControllerTest < ActionController::TestCase
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
+  # test "the truth" do
+  #   assert true
+  # end
 end
