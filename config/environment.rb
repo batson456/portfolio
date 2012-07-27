00ce9dfe5a825740573e84092bc70c953c0a1044
batson456 <batson456@gmail.com>
2012-07-26 22:19:57 -0400

    Add action mailer to environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Portfolio::Application.initialize!
+
+config.action_mailer.delivery_method = :smtp
+ActionMail::Base.server_settings = { :address => "smtp.gmail.com", :port => 25, :user_name => "batson456", :password => "britney12", :authentication => :plain }
