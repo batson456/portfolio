0ebd4747160451b3c16abb395d2d6ec0a57ef622
batson456 <batson456@gmail.com>
2012-07-26 23:09:33 -0400

    Update production.rb to support gmail for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,17 @@ Portfolio::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # Setup the default mailer
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => 'smtp.gmail.com',
+    :port                 => 587,
+    :domain               => 'bbatson.com',
+    :user_name            => 'bryce@bbatson.com',
+    :password             => 'britney12',
+    :authentication       => 'plain',
+    :enable_starttsl_auto => true
+  }
+
 end
